98d3fcc4401d802851e6a24e3e91b23ffd798cb6
Marvin Frede <marvinfrede@gmx.de>
2024-09-01 09:47:15 +0200

    lint: ignore build files & format

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,20 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { ignores: ['vendor/*'] },
+  {
+    ignores: [
+      'vendor/*',
+      'node_modules/*',
+      'dist/*',
+      'build/*',
+      'public/build/*',
+      'public/js/*',
+      'storage/*',
+      '**/*.config.js',
+      'coverage/*',
+      '__snapshots__/*',
+    ],
+  },
   { languageOptions: { globals: globals.browser } },
   { settings: { react: { version: 'detect' } } },
   pluginJs.configs.recommended,
