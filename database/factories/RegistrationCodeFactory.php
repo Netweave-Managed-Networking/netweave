b62bebd3243f8ee90d8af0b84c14d85aec2fb365
Marvin Frede <marvinfrede@gmx.de>
2024-09-10 11:44:29 +0200

    refactor: factory: use Str:random for random string

--- database/factories/RegistrationCodeFactory.php
+++ database/factories/RegistrationCodeFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\RegistrationCode;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\RegistrationCode>
@@ -26,7 +27,7 @@ class RegistrationCodeFactory extends Factory
     public function definition(): array
     {
         return [
-            'code' => $this->faker->bothify('??###?'), // Random 6-character code like 'AB123C'
+            'code' => Str::random(length: 6), // Random 6-character code like 'AB123C'
             'editor_id' => $this->faker->boolean(50) ? User::factory() : null, // 50% chance of being null or a user
             'admin_id' => User::factory(), // Always assigns a valid user as admin
             'created_at' => now(),
