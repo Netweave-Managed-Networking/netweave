ee617ce365c26fe18cd31d49659b7ac05bb8f252
Marvin Frede <marvinfrede@gmx.de>
2024-09-12 12:52:19 +0200

    feat: seeder: generate 10 RegCodes, 1 with or without editor 50/50

--- database/factories/RegistrationCodeFactory.php
+++ database/factories/RegistrationCodeFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserRole;
 use App\Models\RegistrationCode;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -9,6 +10,8 @@ use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\RegistrationCode>
+ *
+ * Needs at least one admin to exist
  */
 class RegistrationCodeFactory extends Factory
 {
@@ -28,8 +31,8 @@ class RegistrationCodeFactory extends Factory
     {
         return [
             'code' => Str::lower(Str::random(length: 8)), // Random 8-character code like 'ab123cde'
-            'editor_id' => $this->faker->boolean(50) ? User::factory() : null, // 50% chance of being null or a user
-            'admin_id' => User::factory(), // Always assigns a valid user as admin
+            'editor_id' => User::factory(), // create a new User as editor
+            'admin_id' => User::where('role', UserRole::ADMIN)->inRandomOrder()->first()->id,
             'created_at' => now(),
             'updated_at' => now(),
         ];
