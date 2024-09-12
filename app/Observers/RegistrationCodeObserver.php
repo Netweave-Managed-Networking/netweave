876b772611e183b901655e03fd34251706781fe7
Marvin Frede <marvinfrede@gmx.de>
2024-09-12 12:51:17 +0200

    refactor: format php

--- app/Observers/RegistrationCodeObserver.php
+++ app/Observers/RegistrationCodeObserver.php
@@ -8,7 +8,7 @@ class RegistrationCodeObserver
 {
     public function deleting(RegistrationCode $registrationCode)
     {
-        if (!is_null($registrationCode->editor_id)) {
+        if (! is_null($registrationCode->editor_id)) {
             throw new \Exception('Deletion not allowed: RegistrationCode belongs to an editor. When the editor is deleted his RegistrationCode will be deleted as well.');
         }
     }
