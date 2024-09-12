829625867b2376bc94271db87f72cc3d5b4f85ff
Marvin Frede <marvinfrede@gmx.de>
2024-09-12 12:10:06 +0200

    feat: RegCodes cannot be deleted when an Editor used it to register

---
+++ app/Observers/RegistrationCodeObserver.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\RegistrationCode;
+
+class RegistrationCodeObserver
+{
+    public function deleting(RegistrationCode $registrationCode)
+    {
+        if (!is_null($registrationCode->editor_id)) {
+            throw new \Exception('Deletion not allowed: RegistrationCode belongs to an editor. When the editor is deleted his RegistrationCode will be deleted as well.');
+        }
+    }
+}
