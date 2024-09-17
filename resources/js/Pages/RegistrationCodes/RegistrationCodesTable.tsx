2d160ed7845ec866e70dcb316dacea7256b02dfc
Marvin Frede <marvinfrede@gmx.de>
2024-09-17 14:28:26 +0200

    feat: RegCode Table: german translation

--- resources/js/Pages/RegistrationCodes/RegistrationCodesTable.tsx
+++ resources/js/Pages/RegistrationCodes/RegistrationCodesTable.tsx
@@ -44,7 +44,7 @@ export default function RegistrationCodesTable({
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
           <Table
-            headerTitles={['code', 'usable', 'used by', 'admin']}
+            headerTitles={['code', 'anwendbar', 'eingeladen', 'angelegt durch']}
             rowItems={tableRows}
           ></Table>
         </div>
