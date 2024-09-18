d8a19c428a3ca2e9484f3eb5974ee7f01da4b911
Marvin Frede <marvinfrede@gmx.de>
2024-09-18 17:27:22 +0200

    chore: remove fixed todo

--- database/migrations/2024_09_01_075849_create_stakeholder_restrictions_table.php
+++ database/migrations/2024_09_01_075849_create_stakeholder_restrictions_table.php
@@ -4,8 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-// TODO better like this or have the stakeholderOrg have a set of retsrictions as one column??
-
 return new class extends Migration
 {
     public function up()
