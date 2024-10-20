e5a23fbf2ef7019aa269bf2a25f1ed110dff0147
Marvin Frede <marvinfrede@gmx.de>
2024-10-20 23:48:36 +0200

    feat: InputLabel can be "required" (shows red astherisk)

--- resources/js/Components/InputLabel.tsx
+++ resources/js/Components/InputLabel.tsx
@@ -3,15 +3,33 @@ import { LabelHTMLAttributes } from 'react';
 export default function InputLabel({
   value,
   className = '',
+  required = false,
   children,
   ...props
-}: LabelHTMLAttributes<HTMLLabelElement> & { value?: string }) {
+}: LabelHTMLAttributes<HTMLLabelElement> & {
+  value?: string;
+  required?: boolean;
+}) {
   return (
-    <label
-      {...props}
-      className={`block font-medium text-sm text-gray-700 ` + className}
-    >
-      {value ? value : children}
-    </label>
+    <div className="flex">
+      <label
+        {...props}
+        className={`block font-medium text-sm text-gray-700 ` + className}
+      >
+        {value ? value : children}
+      </label>
+      {required && (
+        <span
+          className="text-red-700"
+          style={{
+            fontSize: '1.5rem',
+            lineHeight: '1.25rem',
+            margin: '5px 3px -5px',
+          }}
+        >
+          *
+        </span>
+      )}
+    </div>
   );
 }
