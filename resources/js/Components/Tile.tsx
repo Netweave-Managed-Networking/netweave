6a27439c289e0379f15bcc5f0ab32fca4f5b0524
Marvin Frede <marvinfrede@gmx.de>
2024-10-25 16:57:40 +0200

    feat: make `Tile` its own component

---
+++ resources/js/Components/Tile.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+
+export function Tile({ children }: { children?: ReactNode | undefined }) {
+  return (
+    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+      <div
+        className="p-6 text-gray-900 flex gap-2 items-center"
+        style={{ maxHeight: '72px' }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { Tile } from '@/Components/Tile';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { PageProps } from '@/types/page-props.type';
 import { Head } from '@inertiajs/react';
@@ -15,25 +16,15 @@ export default function Dashboard({ auth }: PageProps) {
     >
       <Head title="Dashboard" />
 
-      <div className="pt-12 pb-3">
-        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 text-gray-900">Du bist angemeldet!</div>
-          </div>
+      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div className="pt-12 pb-3">
+          <a href="/stakeholder-organizations/create">
+            <Tile>
+              <AddCircleOutline fontSize="medium" />
+              <span>Neue Organisation anlegen</span>
+            </Tile>
+          </a>
         </div>
-      </div>
-
-      <div className="py-3">
-        <a href="/stakeholder-organizations/create">
-          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-              <div className="p-6 text-gray-900 flex gap-2">
-                <AddCircleOutline fontSize="medium" />
-                <span>Neue Organisation anlegen</span>
-              </div>
-            </div>
-          </div>
-        </a>
       </div>
     </AuthenticatedLayout>
   );
