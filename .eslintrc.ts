a2c56ffd656a9aaf31a11165ea4526f51e3cbc1c
Marvin Frede <marvinfrede@gmx.de>
2024-11-16 18:26:22 +0100

    lint: use prettier for formatting, not eslint

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -23,21 +23,13 @@ const config: Linter.Config = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
   ],
-  plugins: ['prettier'],
   rules: {
     'react/jsx-first-prop-new-line': [2, 'multiline'],
     'react/jsx-max-props-per-line': [2, { maximum: 1, when: 'multiline' }],
     'react/jsx-indent-props': [2, 2],
     'react/jsx-closing-bracket-location': [2, 'tag-aligned'],
-    'prettier/prettier': [
-      'error',
-      {},
-      {
-        usePrettierrc: true,
-      },
-    ],
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     'no-console': 2,
