f53c17ebbf2956c6ed924c6754b9fcb9648a66f6
Marvin Frede <marvinfrede@gmx.de>
2025-03-09 15:14:06 +0100

    refactor(frontend): use SelectAdd to combine functionality from Organization- and ResourceCategorySelectAdd components

--- resources/js/Components/OrganizationCategories/OrganizationCategoriesSelectAdd.tsx
+++ resources/js/Components/OrganizationCategories/OrganizationCategoriesSelectAdd.tsx
@@ -1,11 +1,6 @@
 import { idNameToIdLabel } from '@/helpers/idNameToIdLabel.helper';
-import { IdLabel } from '@/types/id-label.model';
 import { IdName } from '@/types/id-name.model';
-import { OrganizationCategory } from '@/types/organization-category.model';
-import AddIcon from '@mui/icons-material/Add';
-import { Chip } from '@mui/material';
-import { useState } from 'react';
-import BadgeSelect from '../Input/BadgeSelect';
+import SelectAdd from '../Input/SelectAdd';
 import { OrganizationCategoryCreateModal } from './OrganizationCategoryCreateModal';
 
 export interface OrganizationCategoriesSelectAddProps {
@@ -21,59 +16,15 @@ export default function OrganizationCategoriesSelectAdd({
   onChange,
   className = '',
 }: OrganizationCategoriesSelectAddProps) {
-  const [modalIsActive, setModalIsActive] = useState<boolean>(false);
-  const showModal = () => setModalIsActive(true);
-  const hideModal = () => setModalIsActive(false);
-
-  const [catsSelected, setCatsSelected] = useState<IdLabel['id'][]>(
-    organizationCategoriesSelected
-  );
-  const [cats, setCats] = useState<IdLabel[]>(
-    organizationCategories.map(idNameToIdLabel)
-  );
-
-  const updateAndOutput = (newSelected: IdLabel['id'][]) => {
-    setCatsSelected(newSelected);
-    onChange(newSelected);
-  };
-
-  const addCategoryToBadges = (
-    newCategory: OrganizationCategory | undefined
-  ) => {
-    if (newCategory) {
-      organizationCategories.push(newCategory);
-      const newBadge = { ...idNameToIdLabel(newCategory), isActivated: true };
-      setCats([...cats, newBadge]);
-      updateAndOutput([...catsSelected, newBadge.id]);
-    }
-    hideModal();
-  };
-
-  const addButton: JSX.Element = (
-    <Chip
-      icon={<AddIcon />}
-      label="Neue Kategorie"
-      onClick={() => showModal()}
-      sx={{
-        backgroundColor: 'grey.300',
-        '&:hover': { backgroundColor: 'grey.400' },
-      }}
-    />
-  );
-
   return (
-    <div className={className} style={{ marginTop: '4px' }}>
-      <BadgeSelect
-        elements={cats}
-        value={catsSelected}
-        onChange={updateAndOutput}
-        elemAppended={addButton}
-      />
-
-      <OrganizationCategoryCreateModal
-        show={modalIsActive}
-        onClose={addCategoryToBadges}
-      />
-    </div>
+    <SelectAdd
+      items={organizationCategories}
+      itemsSelected={organizationCategoriesSelected}
+      itemToIdLabel={idNameToIdLabel}
+      addSelectableButtonLabel="Neue Kategorie"
+      CreateItemModalComponent={OrganizationCategoryCreateModal}
+      onChange={onChange}
+      className={className}
+    />
   );
 }
