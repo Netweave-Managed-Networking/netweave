ce74888e03bfb5f1579c0375de19c7ef8b0a8ddd
Marvin Frede <marvinfrede@gmx.de>
2025-03-30 17:45:04 +0200

    refactor(frontend): split OrgCreate: OrganizationInput is used which has slots: CategoriesSelect, ContactPerson and Notes

--- resources/js/types/organization-update.model.ts
+++
@@ -1,8 +0,0 @@
-import { Organization } from './organization.model';
-
-export type OrganizationUpdate = Partial<
-  Pick<
-    Organization,
-    'name' | 'email' | 'phone' | 'postcode_city' | 'street_hnr'
-  >
->;
