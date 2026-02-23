87af9fc7831fdae754517b26f3f6da3e98d139ca
Marvin Frede <marvinfrede@gmx.de>
2026-02-23 19:56:07 +0100

    refactor(api): use type WelcomePage

--- apps/netweave-api/src/app/app.service.ts
+++ apps/netweave-api/src/app/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { WelcomeResponseDTO } from '@netweave/api-types';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
+  getData(): WelcomeResponseDTO {
     return { message: 'Hello from API' };
   }
 }
