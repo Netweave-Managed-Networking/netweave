7151da005fa3c74dba49ec6ac5dbbfc9300fc618
Marvin Frede <marvinfrede@gmx.de>
2026-02-23 19:46:57 +0100

    chore(api-types): add lib api-types
    npm run nx g @nx/angular:lib -- --buildable --directory=libs/data-access/api-types

---
+++ libs/data-access/api-types/src/lib/api-types/api-types.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-api-types',
+  imports: [],
+  templateUrl: './api-types.html',
+  styleUrl: './api-types.css',
+})
+export class ApiTypes {}
