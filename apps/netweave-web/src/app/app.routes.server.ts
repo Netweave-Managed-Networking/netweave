037d1b9ecea601643667dd644fdb6f31dd5ed70f
Marvin Frede <mfrede@slashwhy.de>
2026-03-29 21:47:07 +0200

    fix: change to RenderMode.Server for now as long as dummy api call is made on init

--- apps/netweave-web/src/app/app.routes.server.ts
+++ apps/netweave-web/src/app/app.routes.server.ts
@@ -3,6 +3,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Server, // TODO: change back to RenderMode.Prerender once dummy api call is no longer made by app.ts
   },
 ];
