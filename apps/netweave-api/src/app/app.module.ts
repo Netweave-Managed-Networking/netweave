29fda4495f3f80195a2d2334a7c33cb9391cd72f
Marvin Frede <mfrede@slashwhy.de>
2026-04-11 00:22:59 +0200

    fix(netweave-api): introduce db host

--- apps/netweave-api/src/app/app.module.ts
+++ apps/netweave-api/src/app/app.module.ts
@@ -13,11 +13,11 @@ import { MailService } from './mail.service';
     HttpModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: 5432,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DB,
       entities: [],
       synchronize: true, // TODO: Set to false in production
     }),
