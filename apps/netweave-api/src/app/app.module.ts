5ef7bc9bfd442efae11558f1964bfa8221142bd6
Marvin Frede <mfrede@slashwhy.de>
2026-04-10 18:49:18 +0200

    feat(netweave-api): add netweave-db and connect to it via TypeORM

--- apps/netweave-api/src/app/app.module.ts
+++ apps/netweave-api/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FetchService } from './fetch.service';
 import { MailService } from './mail.service';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), HttpModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    HttpModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true, // TODO: Set to false in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, FetchService, MailService],
 })
-export class AppModule { }
+export class AppModule {}
