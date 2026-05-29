406ef756b369783e15261e4cc27b5d54a0786ba3
Marvin Frede <mfrede@slashwhy.de>
2026-05-29 23:30:46 +0200

    refactor(api-e2e): add GlobalThis for TEARDOWN_MESSAGE

--- apps/netweave-api-e2e/src/support/global-setup.ts
+++ apps/netweave-api-e2e/src/support/global-setup.ts
@@ -1,7 +1,10 @@
 import { waitForPortOpen } from '@nx/node/utils';
 
-/* eslint-disable */
-var __TEARDOWN_MESSAGE__: string;
+declare global {
+  interface GlobalThis {
+    __TEARDOWN_MESSAGE__?: string;
+  }
+}
 
 module.exports = async function () {
   // Start services that that the app needs to run (e.g. database, docker-compose, etc.).
@@ -10,5 +13,5 @@ module.exports = async function () {
   await waitForPortOpen(3000, { host: 'localhost' });
 
   // Hint: Use `globalThis` to pass variables to global teardown.
-  (globalThis as any).__TEARDOWN_MESSAGE__ = '\nTearing down...\n';
+  (globalThis as GlobalThis).__TEARDOWN_MESSAGE__ = '\nTearing down...\n';
 };
--- apps/netweave-api-e2e/src/support/global-teardown.ts
+++ apps/netweave-api-e2e/src/support/global-teardown.ts
@@ -1,8 +1,14 @@
 import { killPort } from '@nx/node/utils';
 
+declare global {
+  interface GlobalThis {
+    __TEARDOWN_MESSAGE__?: string;
+  }
+}
+
 module.exports = async function () {
   // Put clean up logic here (e.g. stopping services, docker-compose, etc.).
   // Hint: `globalThis` is shared between setup and teardown.
   await killPort(3000);
-  console.log(globalThis.__TEARDOWN_MESSAGE__);
+  console.log((globalThis as GlobalThis).__TEARDOWN_MESSAGE__);
 };
