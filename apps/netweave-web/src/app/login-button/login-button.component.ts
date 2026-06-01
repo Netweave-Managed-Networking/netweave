f7735fd493d30b34f608fa4e7accbd5b724c596d
Marvin Frede <mfrede@slashwhy.de>
2026-06-01 21:56:16 +0200

    feat(web): user login & registration

---
+++ apps/netweave-web/src/app/login-button/login-button.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-login-button',
+  standalone: true,
+  imports: [RouterLink],
+  styleUrls: ['./login-button.component.scss'],
+  templateUrl: './login-button.component.html',
+})
+export class LoginButtonComponent {
+  private auth = inject(AuthService);
+
+  protected isLoggedIn = signal<boolean>(this.auth.isLoggedIn());
+
+  protected logout() {
+    this.auth.logout();
+  }
+}
---
+++ apps/netweave-web/src/app/welcome-user/welcome-user.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcome-user.component.html',
+})
+export class WelcomeUserComponent {}
