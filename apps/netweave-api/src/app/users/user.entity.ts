044a99a1c1bce30c70f7404641f1f435a4e0d3fc
Marvin Frede <mfrede@slashwhy.de>
2026-06-01 15:25:52 +0200

    feat(api): user login & registration

---
+++ apps/netweave-api/src/app/users/user.entity.ts
@@ -0,0 +1,11 @@
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from '../db/entity/base/base.entity';
+
+@Entity()
+export class User extends BaseEntity {
+  @Column({ unique: true })
+  declare email: string;
+
+  @Column()
+  declare passwordHash: string;
+}
