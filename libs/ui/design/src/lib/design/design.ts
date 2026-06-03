5a61923748c1864380479997097c595697034f1d
Marvin Frede <mfrede@slashwhy.de>
2026-06-03 14:58:29 +0200

    chore(api-types): add lib design
    npm run nx g @nx/angular:lib -- --buildable --directory=libs/ui/design

---
+++ libs/ui/design/src/lib/design/design.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-design',
+  imports: [],
+  templateUrl: './design.html',
+  styleUrl: './design.css',
+})
+export class Design {}
