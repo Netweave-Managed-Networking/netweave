e99098707b25853910677956ec27863aa57d11c9
Marvin Frede <mfrede@slashwhy.de>
2026-07-16 12:30:26 +0200

    feat(design)!: remove icons "info", "trash"

--- libs/ui/design/eslint.config.mjs
+++ libs/ui/design/eslint.config.mjs
@@ -26,7 +26,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'lib',
+          prefix: 'design',
           style: 'camelCase',
         },
       ],
--- libs/ui/design/src/lib/design/icons/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './info/info.icon';
-export * from './trash/trash.icon';
