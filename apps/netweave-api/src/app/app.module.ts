f5fba9cab8271f8b55157cacab1bc0e76c87c221
Marvin Frede <mfrede@slashwhy.de>
2026-07-16 22:00:48 +0200

    feat(api): remove welcome-api
    delete app.service and app.controller since their only task was welcome-api

--- apps/netweave-api/src/app/app.module.ts
+++ apps/netweave-api/src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 import { AuthModule } from './auth/auth.module';
 import { OrganizationsModule } from './organizations/organizations.module';
@@ -14,7 +12,5 @@ import { MailModule } from './utils/mail.service/mail.module';
     AuthModule,
     UserEmailWhitelistsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- apps/netweave-api/src/app/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { WelcomeResponseDTO } from '@netweave/api-types';
-
-@Injectable()
-export class AppService {
-  public getData(): WelcomeResponseDTO {
-    return { message: 'Hello from API' };
-  }
-}
