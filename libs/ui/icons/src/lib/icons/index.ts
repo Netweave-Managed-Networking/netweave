c7bc74732e6150f70cdf5fdf55268cec9009c592
Marvin Frede <mfrede@slashwhy.de>
2026-07-16 13:18:06 +0200

    feat(icons): add icon "sidebar"

--- libs/ui/icons/src/lib/icons/index.ts
+++ libs/ui/icons/src/lib/icons/index.ts
@@ -2,4 +2,5 @@ export * from './home/icon.home';
 export * from './info/icon.info';
 export * from './plus/icon.plus';
 export * from './settings/icon.settings';
+export * from './sidebar/icon.sidebar';
 export * from './trash/icon.trash';
