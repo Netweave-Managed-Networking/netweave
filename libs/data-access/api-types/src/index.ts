735ce8300378ff0eb5d20b89ee4645522cb107db
Marvin Frede <mfrede@slashwhy.de>
2026-07-16 22:01:14 +0200

    feat(api-types)!: remove unused welcome-dto

--- libs/data-access/api-types/src/index.ts
+++ libs/data-access/api-types/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/api-types/organization';
 export * from './lib/api-types/user';
 export * from './lib/api-types/user-email-whitelist';
-export * from './lib/api-types/welcome';
--- libs/data-access/api-types/src/lib/api-types/welcome/welcome-message.dto.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface WelcomeResponseDTO {
-  message: string;
-}
