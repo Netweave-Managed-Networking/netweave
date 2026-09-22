aed8455f7fb7e6eb5cede73f6d4f86f372c0af18
Henk Hornschuh <h.hornschuh@basecom.de>
2026-09-22 16:34:22 +0200

    feat(api-types): add member email dtos

--- libs/data-access/api-types/src/lib/api-types/invitation/index.ts
+++ libs/data-access/api-types/src/lib/api-types/invitation/index.ts
@@ -1,3 +1,4 @@
 export * from './invitation-list-item.dto.interface';
+export * from './invitation-token.dto.interface';
 export * from './invitation.create.dto.class';
 export * from './invitation.dto.interface';
