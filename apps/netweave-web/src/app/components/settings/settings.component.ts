ed2dab5c449667ce45105018e407683b36cd4bdd
Henk Hornschuh <h.hornschuh@basecom.de>
2026-09-24 14:19:39 +0200

    feat(web): add admin user overview

--- apps/netweave-web/src/app/components/settings/settings.component.ts
+++ apps/netweave-web/src/app/components/settings/settings.component.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { IconMail, IconSettings } from '@netweave/icons';
 import { MailSettingsComponent } from '../mail-settings/mail-settings.component';
 import { UserInvitationsComponent } from '../user-invitations/user-invitations.component';
+import { UserRolesComponent } from '../user-roles/user-roles.component';
 
 type SettingsTab = 'manager' | 'mail';
 
@@ -11,6 +12,7 @@ type SettingsTab = 'manager' | 'mail';
     IconMail,
     IconSettings,
     UserInvitationsComponent,
+    UserRolesComponent,
     MailSettingsComponent,
   ],
   templateUrl: './settings.component.html',
